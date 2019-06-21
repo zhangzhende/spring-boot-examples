a9afee54b994f175f7f3da396366111d2ac79e47
zzd <zzd@163.com>
2019-06-21 09:22:37 +0800

    更新代码

---
+++ service-spark/src/main/java/com/zzd/spark/ServiceSparkApplication.java
@@ -0,0 +1,13 @@
+package com.zzd.spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSparkApplication.class, args);
+    }
+
+}
