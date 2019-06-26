082496114b8f0c99ea95d2ed5f90f6367d832ecd
zzd <zzd@163.com>
2019-06-26 10:32:56 +0800

    更新代码

---
+++ service-esrestclient/src/main/java/com/zzd/restclient/ServiceEsRestclientApplication.java
@@ -0,0 +1,13 @@
+package com.zzd.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEsRestclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEsRestclientApplication.class, args);
+    }
+
+}
---
+++ service-esrestclient/src/test/java/com/zzd/restclient/ServiceEsRestclientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zzd.restclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceEsRestclientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
